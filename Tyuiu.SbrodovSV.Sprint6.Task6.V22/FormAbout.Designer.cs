025d97984a2faa6324cc47988ed59cb7683f0cad
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-12-08 16:45:10 +0500

    Task6

--- Tyuiu.SbrodovSV.Sprint6.Task6.V22/FormAbout.Designer.cs
+++ Tyuiu.SbrodovSV.Sprint6.Task6.V22/FormAbout.Designer.cs
@@ -31,8 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
             this.labelInfo_SSV = new System.Windows.Forms.Label();
             this.buttonOK_SSV = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxAvatar_SSV = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar_SSV)).BeginInit();
             this.SuspendLayout();
             // 
             // labelInfo_SSV
@@ -54,22 +54,22 @@
             this.buttonOK_SSV.UseVisualStyleBackColor = true;
             this.buttonOK_SSV.Click += new System.EventHandler(this.buttonOK_SSV_Click);
             // 
-            // pictureBox1
+            // pictureBoxAvatar_SSV
             // 
-            this.pictureBox1.Image = global::Tyuiu.SbrodovSV.Sprint6.Task6.V22.Properties.Resources._7XnkBGmKW4k4EFWs7372cA;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(117, 78);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxAvatar_SSV.Image = global::Tyuiu.SbrodovSV.Sprint6.Task6.V22.Properties.Resources._7XnkBGmKW4k4EFWs7372cA;
+            this.pictureBoxAvatar_SSV.Location = new System.Drawing.Point(12, 39);
+            this.pictureBoxAvatar_SSV.Name = "pictureBoxAvatar_SSV";
+            this.pictureBoxAvatar_SSV.Size = new System.Drawing.Size(117, 78);
+            this.pictureBoxAvatar_SSV.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxAvatar_SSV.TabIndex = 2;
+            this.pictureBoxAvatar_SSV.TabStop = false;
             // 
             // FormAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(432, 165);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxAvatar_SSV);
             this.Controls.Add(this.buttonOK_SSV);
             this.Controls.Add(this.labelInfo_SSV);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -78,7 +78,7 @@
             this.Name = "FormAbout";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "О программе";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar_SSV)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +88,6 @@
 
         private System.Windows.Forms.Label labelInfo_SSV;
         private System.Windows.Forms.Button buttonOK_SSV;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxAvatar_SSV;
     }
 }
